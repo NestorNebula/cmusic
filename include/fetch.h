33043bdf525ce62c2fc9ec83a994ece211b986a1
NestorNebula <160883075+NestorNebula@users.noreply.github.com>
2025-06-16 09:58:53 +0200

    feat: include fetch module's header

---
+++ include/fetch.h
@@ -0,0 +1,12 @@
+#include <cjson/cJSON.h>
+#include "types.h"
+
+/*
+ * fetch:
+ * Fetches data using the provided URL and method.
+ * If body isn't null, includes it as the body of the request.
+ * The method argument must be one of "GET", "POST", "PUT" and "DELETE".
+ * Once the data has been fetched, parses it using cJSON module.
+ * Returns the parsed data if no error occurred, else returns a null pointer.
+ */
+cJSON *fetch(string url, string method, string body);
