1d6b6f20299337f8bdf67532cd7a9d8493b9d46f
NestorNebula <160883075+NestorNebula@users.noreply.github.com>
2025-06-17 11:48:44 +0200

    docs: update fetch function description

--- include/fetch.h
+++ include/fetch.h
@@ -10,7 +10,9 @@
  * If body isn't null, includes it as the body of the request.
  * The method argument must be one of "GET", "POST", "PUT" and "DELETE".
  * Once the data has been fetched, parses it using cJSON module.
- * Returns the parsed data if no error occurred, else returns a null pointer.
+ * Returns the parsed data if no error occurred, else terminates program.
+ * The returned pointer can be a null pointer if no data was returned in
+ * the request's response, but no error occurred during the request.
  */
 cJSON *fetch(string url, string method, string body);
 
