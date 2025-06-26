2215dab6954508483c8feb549c97597c7ceb10ce
NestorNebula <160883075+NestorNebula@users.noreply.github.com>
2025-06-26 09:51:44 +0200

    refactor: update module organization
    Function-like macros will now call real functions depending on their arguments

--- include/tprint.h
+++ include/tprint.h
@@ -1,147 +1,199 @@
 #ifndef TPRINT_H
 #define TPRINT_H
 
-#include <stdio.h>
 #include "types.h"
+
 
 /*
  * Type Structures Printers functions:
- * The printers are implemented as function-like macros to allow types
- * that have many properties in common to call the same function.
- * Each type has a "print details" and a "print essentials" function.
- * If a type is incompatible with the details function, it will be redirected
- * toward the essentials function. If it is incompatible with the essentials
- * function, nothing will be printed.
- * A more general function is also provided for each type. Depending of its
- * argument, it will decide to call the details or the essentials function.
- * As functions expect particular types as arguments but are implemented
- * as function-like macros, calling these functions with unexpected types
- * will result in undefined behavior.
+ * The printers wrappers are implemented as function-like macros to allow types
+ * that have many properties in common to call the same function-like macro.
+ * Each type has "reals" function "print details" and "print essentials",
+ * called by the function-like macros depending on the values of their
+ * arguments.
  */
 
 /*
  * print_array:
  * Prints array as a list with a number as index (one-indexed) for each item.
- * Prints each array item using item_printer.
- * Expects a null terminated array as argument. The array's items must have a
+ * Prints each array item by calling item_printer with with_details and
+ * is_simplified as arguments.
+ * Expects a null terminated array as first argument. The array's items must have a
  * type expected by the item_printer function.
  */
-#define print_array(array, item_printer)
+#define print_array(array, item_printer, with_details, is_simplified) \
 
 
 /*
  * print_album:
- * Calls print_album_details if album has type Album and has no missing
- * information. Else, calls print_album_essentials.
+ * Calls print_album_details, print_album_essentials,
+ * print_simplified_album_details or print_simplified_album_essentials
+ * depending on the values of with_details and is_simplified.
  * Expects an Album or SimplifiedAlbum argument.
  */
-#define print_album(album)
+#define print_album(album, with_details, is_simplified) \
 
 /*
  * print_album_details:
  * Prints album with detailed informations.
- * Expects an Album or SimplifiedAlbum argument.
  */
-#define print_album_details(album)
+void print_album_details(Album album);
 
 /*
  * print_album_essentials:
  * Prints album's essential informations.
- * Expects an Album or SimplifiedAlbum argument.
  */
-#define print_album_essentials(album)
+void print_album_essentials(Album album);
+
+/*
+ * print_simplified_album_details:
+ * Prints simplified album with detailed informations.
+ */
+void print_simplified_album_details(SimplifiedAlbum simplified_album);
+
+/*
+ * print_simplified_album_essentials:
+ * Prints simplified album's essential informations.
+ */
+void print_simplified_album_essentials(SimplifiedAlbum simplified_album);
 
 
 /*
  * print_artist:
- * Calls print_artist_details if artist has type Artist and has no missing
- * information. Else, calls print_artist_essentials.
+ * Calls print_artist_details, print_artist_essentials,
+ * print_simplified_artist_details or print_simplified_artist_essentials
+ * depending on the values of with_details and is_simplified.
  * Expects an Artist or SimplifiedArtist argument.
  */
-#define print_artist(artist)
+#define print_artist(artist, with_details, is_simplified) \
 
 /*
  * print_artist_details:
  * Prints artist with detailed informations.
- * Expects an Artist argument.
  */
-#define print_artist_details(artist)
+void print_artist_details(Artist artist);
 
 /*
  * print_artist_essentials:
  * Prints artist's essential informations.
- * Expects an Artist or SimplifiedArtist argument.
  */
-#define print_artist_essentials(artist)
+void print_artist_essentials(Artist artist);
+
+/*
+ * print_simplified_artist_details:
+ * Prints simplified artist with detailed informations.
+ */
+void print_simplified_artist_details(SimplifiedArtist simplified_artist);
+
+/*
+ * print_simplified_artist_essentials:
+ * Prints simplified artist's essential informations.
+ */
+void print_simplified_artist_essentials(SimplifiedArtist simplified_artist);
 
 
 /*
  * print_playlist:
- * Calls print_playlist_details if playlist has type Playlist and has no
- * missing information. Else, calls print_playlist_essentials.
+ * Calls print_playlist_details, print_playlist_essentials,
+ * print_simplified_playlist_details or print_simplified_playlist_essentials
+ * depending on the values of with_details and is_simplified.
  * Expects a Playlist or SimplifiedPlaylist argument.
  */
-#define print_playlist(playlist)
+#define print_playlist(playlist, with_details, is_simplified) \
 
 /*
  * print_playlist_details:
  * Prints playlist with detailed informations.
- * Expects a Playlist or SimplifiedPlaylist argument.
  */
-#define print_playlist_details(playlist)
+void print_playlist_details(Playlist playlist);
 
 /*
  * print_playlist_essentials:
  * Prints playlist's essential informations.
- * Expects a Playlist or SimplifiedPlaylist argument.
  */
-#define print_playlist_essentials(playlist)
+void print_playlist_essentials(Playlist playlist);
+
+/*
+ * print_simplified_playlist_details:
+ * Prints simplified playlist with detailed informations.
+ */
+void print_simplified_playlist_details(SimplifiedPlaylist simplified_playlist);
+
+/*
+ * print_simplified__playlist_essentials:
+ * Prints simplified playlist's essential informations.
+ */
+void print_simplified_playlist_essentials(SimplifiedPlaylist
+                                            simplified_playlist);
 
 
 /*
  * print_track:
- * Calls print_track_details if track has type Track and has no missing
- * information. Else, calls print_track_essentials.
+ * Calls print_track_details, print_track_essentials,
+ * print_simplified_track_details or print_simplified_track_essentials
+ * depending on the values of with_details and is_simplified.
  * Expects a Track or SimplifiedTrack argument.
  */
-#define print_track(track)
+#define print_track(track) \
 
 /*
  * print_track_details:
  * Prints track with detailed informations.
- * Expects a Track or SimplifiedTrack argument.
  */
-#define print_track_details(track)
+void print_track_details(Track track);
 
 /*
  * print_track_essentials:
  * Prints track's essential informations.
- * Expects a Track or SimplifiedTrack argument.
  */
-#define print_track_essentials(track)
+void print_track_essentials(Track track);
+
+/*
+ * print_simplified_track_details:
+ * Prints simplified track with detailed informations.
+ */
+void print_simplified_track_details(SimplifiedTrack simplified_track);
+
+/*
+ * print_simplified_track_essentials:
+ * Prints simplified track's essential informations.
+ */
+void print_simplified_track_essentials(SimplifiedTrack simplified_track);
 
 
 /*
  * print_user:
- * Calls print_user_details if user has type User and has no missing 
- * information. Else, calls print_user_essentials.
+ * Calls print_user_details, print_user_essentials,
+ * print_simplified_user_details or print_simplified_user_essentials depending
+ * on tha values of with_details and is_simplified.
  * Expects a User or SimplifiedUser argument.
  */
-#define print_user(user)
+#define print_user(user, with_details, is_simplified) \
 
 /*
  * print_user_details:
  * Prints user with detailed informations.
- * Expects a User argument.
  */
-#define print_user_details(user)
+void print_user_details(User user);
 
 /*
  * print_user_essentials:
  * Prints user's essential informations.
- * Expect a User or SimplifiedUser argument.
  */
-#define print_user_essentials(user)
+void print_user_essentials(User user);
+
+/*
+ * print_simplified_user_details:
+ * Prints user with detailed informations.
+ */
+void print_simplified_user_details(SimplifiedUser simplified_user);
+
+/*
+ * print_simplified_user_essentials:
+ * Prints simplified user's essential informations.
+ */
+void print_simplified_user_essentials(SimplifiedUser simplified_user);
+
 
 FILE *print_stream;
 
