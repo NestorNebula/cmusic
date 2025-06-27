cd24483ff989579e287e67f9c17bdfa6d2975d57
NestorNebula <160883075+NestorNebula@users.noreply.github.com>
2025-06-27 09:26:37 +0200

    feat: add readers module's header

---
+++ include/readers.h
@@ -0,0 +1,27 @@
+#include "types.h"
+
+/*
+ * read_string:
+ * Reads string from stream. Starts at the first non-space character and.
+ * stops when meeting a new-line character or end-of-file.
+ * Returns the string read, as a dynamically allocated string.
+ */
+string read_string(FILE *stream);
+
+/*
+ * read_integer:
+ * Reads an integer from stream and returns it.
+ * If an integer could be read, sets variable pointed by success to true,
+ * else sets it to false.
+ * If no number could be read, the returned value is undefined, thus, it it
+ * important to check that a number was read, using the variable pointed 
+ * by success.
+ */
+int read_integer(FILE *stream, bool *success);
+
+/*
+ * read_bool:
+ * Reads stream and returns true if the first non-space character is a y,
+ * else returns false.
+ */
+bool read_bool(FILE *stream);
