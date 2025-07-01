972255eba2de1d2728a56331bd7004e266eb13fb
NestorNebula <160883075+NestorNebula@users.noreply.github.com>
2025-07-01 15:51:36 +0200

    feat: add functions declarations to helpers header

--- include/helpers.h
+++ include/helpers.h
@@ -11,14 +11,25 @@
  * with this data or with the user.
  */
 
-// Authenticated user
-extern User user;
-
 /*
  * Prints options_count options provided as arguments.
  * Prompts the user to choose an option. Returns the index of the selected
  * option if the index is valid, else returns -1.
  */
 int handle_option_choice(size_t options_count, ...);
+
+/*
+ * update_playlists:
+ * Queries the API for user's owned and followed playlists and uses
+ * the response to update owned_playlists and followed_playlists.
+ */
+void update_playlists(void);
+
+/*
+ * update_followed_artists:
+ * Queries the API for the user's followed artists and uses the response 
+ * to update followed_artists.
+ */
+void update_followed_artists(void);
 
 #endif
